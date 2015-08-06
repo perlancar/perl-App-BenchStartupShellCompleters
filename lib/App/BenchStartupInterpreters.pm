1466d3fea0f252ca292c54cc1f7e984991eaf67c
perlancar (@pc-office) <perlancar@gmail.com>
2015-08-06 17:44:19 +0700

    Initial commit

---
+++ lib/App/BenchStartupInterpreters.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupInterpreters;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of various interpreters
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-interpreters>.
